868a2b0719de59d9d6639af40370f2538d4f17e9
Sriharshini Chitluri <srichitluri@gmail.com>
2022-09-25 20:27:13 -0400

    Fixed the CSS and JS according to updates to HTML
    streamlined JS and added API to weather icon. Added temperature toggle btw C and F

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-// feature 1, get current weekday and time
-function formatDate(date) {
+function formatDate(timestamp) {
+  // timestamp is going to get the number of milliseconds since 1970, what is shown by the API JS
+  // this function will calculate the date
+  let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -8,7 +10,6 @@ function formatDate(date) {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  let dayIndex = date.getDay();
   let days = [
     "Sunday",
     "Monday",
@@ -18,73 +19,90 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
-  let day = days[dayIndex];
+  let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
-let dateElement = document.querySelector("#date");
-let currentTime = new Date();
-dateElement.innerHTML = formatDate(currentTime);
 
-// feature 2 - add a search engine, searched city replaces city name on display after submission
-function displayWeatherCondition(reponse) {
-  document.querySelector("#city-select").innerHTML = reponse.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    reponse.data.main.temp
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
+  celsiusTemperature = response.data.main.temp; // sets the value of the global function
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000); // the dt * 1000 converts the 3 into milliseconds
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
-  document.querySelector("#humidity").innerHTML = reponse.data.main.humidity;
-  document.querySelector("#wind").innerHTML = Math.round(
-    reponse.data.wind.speed
-  );
-  document.querySelector("#description").innerHTML =
-    reponse.data.weather[0].main;
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-function searchCity(city) {
+function search(city) {
   let apiKey = "855cb9e221f20919b83b702e5cef27e2";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeatherCondition);
+  axios.get(apiUrl).then(displayTemperature);
 }
 
-function handleSubmit(event) {
-  //debugger;
-  event.preventDefault();
-  // make an API call to OpenWeather API
-  // Once get the HTTP response, display the city name and the temperature
-  let city = document.querySelector("#city-input").value;
-  searchCity(city);
-}
 function searchLocation(position) {
   let apiKey = "855cb9e221f20919b83b702e5cef27e2";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
+
 function getCurrentLocation(event) {
   event.preventDefault(); // prevent default behavior
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", handleSubmit);
-//searchForm.addEventListener("keypress", search(event) {if (event.key ==='Enter') {}});
+function handleSubmit(event) {
+  event.preventDefault(); // prevent page from reloading
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+function displayFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  // need to remove the active class from celsius link
+  celsiusLink.classList.remove("active");
+  // then add the active class to fahrenheit link
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemp = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemp);
+}
+
+function displayCelsius(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+// global variables
+
+let celsiusTemperature = null;
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
 
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
-searchCity("New York");
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheit);
 
-// feature 3 bonus - clicking on C or F hyperlinks changes the temperature accordingly
-function tempUnitF(event) {
-  event.preventDefault();
-  let currentTemp = document.querySelector(".temperature2");
-  currentTemp.innerHTML = `75`;
-}
-let tempTypeF = document.querySelector("#fahrenheit-link");
-tempTypeF.addEventListener("click", tempUnitF);
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsius);
 
-function tempUnitC(event) {
-  event.preventDefault();
-  let currentTemp = document.querySelector(".temperature2");
-  currentTemp.innerHTML = `24`;
-}
-let tempTypeC = document.querySelector("#celsius-link");
-tempTypeC.addEventListener("click", tempUnitC);
+search("New York");
