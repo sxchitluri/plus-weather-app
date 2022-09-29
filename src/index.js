38e90210e5d48f7febb614b3a3d9fa5fa0eecaed
Sriharshini Chitluri <srichitluri@gmail.com>
2022-09-29 19:26:43 -0400

    moved the inner HTML of weather-forecast Class in forecast card body into JS. Used innerHTML and forEach loop to have the row of columns repeat for an array of days

--- src/index.js
+++ src/index.js
@@ -23,6 +23,31 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Sun", "Mon", "Tues"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img src="" alt="" width="42" />
+        <div class="weather-forecast-temp">
+          <span class="weather-forecast-temp-max"> 18° </span>
+          <span class="weather-forecast-temp-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -106,3 +131,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsius);
 
 search("New York");
+displayForecast();
